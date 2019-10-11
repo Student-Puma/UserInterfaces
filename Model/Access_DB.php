303602de6ce1628558f3234d2c1f3e2946c19996
Gatete ROG <kikefontanlorenzo@gmail.com>
2019-10-11 20:35:29 +0200

    :zap: Relations in a hard and dirty way!

--- Model/Access_DB.php
+++ Model/Access_DB.php
@@ -9,6 +9,52 @@
 	include_once '../Model/config.php';
 
 	/**
+	 * Genera las dependencias de la BD
+     * Es un método 'toy' enfocado en el desarrollo,
+     * con el fin de poder aportar una solución en el plazo
+     * fijado. ¡Nunca llevarlo a producción!
+	 * 
+	 * @var db Conexión con la base de datos
+	 */
+    function RelationDB($db)
+    {        
+        // Relacionamos EDIFICIO <- CENTRO
+        $sql = "ALTER TABLE CENTRO
+                ADD CONSTRAINT FK_EDIFICIOCENTRO
+                FOREIGN KEY (CODEDIFICIO) REFERENCES EDIFICIO(CODEDIFICIO);";
+		// Relacionamos EDIFICIO <- ESPACIO
+        $sql+= "ALTER TABLE ESPACIO
+                ADD CONSTRAINT FK_EDIFICIOESPACIO
+                FOREIGN KEY (CODEDIFICIO) REFERENCES EDIFICIO(CODEDIFICIO);";
+		// Relacionamos CENTRO <- ESPACIO
+        $sql+= "ALTER TABLE ESPACIO
+                ADD CONSTRAINT FK_CENTROESPACIO
+                FOREIGN KEY (CODCENTRO) REFERENCES CENTRO(CODCENTRO);";
+		// Relacionamos PROFESOR <-> ESPACIO
+        $sql+= "ALTER TABLE PROF_ESPACIO
+                ADD CONSTRAINT FK_PROF_ESPACIO
+				FOREIGN KEY (CODESPACIO) REFERENCES ESPACIO(CODESPACIO);
+				
+				ALTER TABLE PROF_ESPACIO
+                ADD CONSTRAINT FK_ESPACIO_PROF
+                FOREIGN KEY (DNI) REFERENCES PROFESOR(DNI);";
+		// Relacionamos PROFESOR <-> TITULACION
+        $sql+= "ALTER TABLE PROF_TITULACION
+                ADD CONSTRAINT FK_PROF_TITULACION
+				FOREIGN KEY (CODTITULACION) REFERENCES TITULACION(CODTITULACION);
+				
+				ALTER TABLE PROF_TITULACION
+                ADD CONSTRAINT FK_TITULACION_PROF
+				FOREIGN KEY (DNI) REFERENCES PROFESOR(DNI);";
+		// Relacionamos CENTRO <- TITULACION
+		$sql+= "ALTER TABLE TITULACION
+                ADD CONSTRAINT FK_CENTROTITULACION
+                FOREIGN KEY (CODCENTRO) REFERENCES CENTRO(CODCENTRO);";
+		// Actualizamos las relaciones
+		@$db->query($sql);
+    }
+
+	/**
 	 * Conecta con la base de datos
 	 * En caso de error, muestra el mensaje correspondiente
 	 * 
@@ -31,6 +77,9 @@
 		}
 		else
 		{
+			// Relacionamos la BD
+			RelationDB($mysqli);
+
 			// Devolvemos la conexión creada
 			return $mysqli;
 		}
