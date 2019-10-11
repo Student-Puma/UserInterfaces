8d7b2913c861d80be6c17b831fa6cfc33d660728
Gatete ROG <kikefontanlorenzo@gmail.com>
2019-10-11 13:01:42 +0200

    :pencil2: Comments! Part III (Models commented)

--- Model/PROFESOR_Model.php
+++ Model/PROFESOR_Model.php
@@ -1,176 +1,215 @@
-
 <?php
+	/**
+	 * Autor: Diego Enrique Fontán Lorenzo
+	 * DNI: 77482941N
+	 */
 
-//	Clase : EDIFICIO_Model
-//	Creado el : 09-10-2017
-//	Creado por: Diego E. Fontan
-//-------------------------------------------------------
-
-class PROFESOR_Model {
-	var $dni;
-	var $nombre;
-	var $apellidos;
-	var $area;
-	var $departamento;
-
-	//	Constructor de la clase
-	//
-
-	function __construct($dni,$nombre,$apellidos,$area,$departamento){
-		$this->dni = $dni;
-		$this->nombre = $nombre;
-		$this->apellidos = $apellidos;
-		$this->area = $area;
-		$this->departamento = $departamento;
-		$this->erroresdatos = array(); 
-
-		//$this->Comprobar_atributos();
-
-		include_once '../Model/Access_DB.php';
-		$this->mysqli = ConnectDB();
-	}
-
-	//Metodo ADD
-	//Inserta en la tabla  de la bd  los valores
-	// de los atributos del objeto. Comprueba si la clave/s esta vacia y si 
-	//existe ya en la tabla
-	function ADD()
+	/**
+	 * Modelo de la entidad PROFESOR
+	 * 
+	 * @var dni DNI del profesor (PK)
+	 * @var nombre Nombre del profesor
+	 * @var apellidos Apellidos del profesor
+	 * @var area Área de conocimiento del profesor
+	 * @var departamento Departamento del profesor
+	 * 
+	 * @var mysqli Conexión con la BD
+	 */
+	class PROFESOR_Model
 	{
+		// Variables de la clase
+		var $dni;
+		var $nombre;
+		var $apellidos;
+		var $area;
+		var $departamento;
+
+		/**
+		 * Constructor de la clase
+		 */
+		function __construct($dni,$nombre,$apellidos,$area,$departamento){
+			$this->dni = $dni;
+			$this->nombre = $nombre;
+			$this->apellidos = $apellidos;
+			$this->area = $area;
+			$this->departamento = $departamento;
+
+			$this->erroresdatos = array(); // FIX: ? Unused variable
+
+			// Añadimos el modelo de acceso a la base de datos
+			include_once '../Model/Access_DB.php';
+			// Nos conectamos con la base de datos
+			$this->mysqli = ConnectDB();
+		}
+
+		/**
+		 * Destructor de la clase
+		 */
+		function __destruct()
+		{ }
+
+		/**
+		 * Inserta valores en la BD
+		 * Comprueba si la clave está vacía o si ya existe en la tabla
+		 * 
+		 * @return msg Mensaje correspondiente al resultado
+		 */
+		function ADD()
+		{
+			// Consulta SQL
 			$sql = "select * from PROFESOR where DNI = '".$this->dni."'";
 
+			// Ejecuta la consulta
 			if (!$result = $this->mysqli->query($sql))
 			{
 				return 'Error de gestor de base de datos';
 			}
 
-			if ($result->num_rows == 1){  // existe el usuario
-					return 'Inserción fallida: el elemento ya existe';
-				}
+			// Comprueba si ya existe la clave
+			if ($result->num_rows == 1)
+			{
+				return 'Inserción fallida: el elemento ya existe';
+			}
 
+			// Consulta SQL
 			$sql = "INSERT INTO PROFESOR (
-				DNI,
-				NOMBREPROFESOR,
-				APELLIDOSPROFESOR,
-				AREAPROFESOR,
-				DEPARTAMENTOPROFESOR) 
+						DNI,
+						NOMBREPROFESOR,
+						APELLIDOSPROFESOR,
+						AREAPROFESOR,
+						DEPARTAMENTOPROFESOR) 
 					VALUES (
 						'".$this->dni."',
 						'".$this->nombre."',
 						'".$this->apellidos."',
 						'".$this->area."',
 						'".$this->departamento."'
-						)";
+					)";
 
-			if (!$this->mysqli->query($sql)) {
+			// Ejecutamos la sentencia y devolvemos
+			// el mensaje correspondiente
+			if (!$this->mysqli->query($sql))
+			{
 				return 'Error de gestor de base de datos';
 			}
-			else{
-				return 'Inserción realizada con éxito'; //operacion de insertado correcta
+			else
+			{
+				return 'Inserción realizada con éxito';
 			}		
-	}
-		
-
-	//funcion de destrucción del objeto: se ejecuta automaticamente
-	//al finalizar el script
-	function __destruct()
-	{
-
-	}
-
-	/**
-	 * Busca valores en la BD
-	 * 
-	 * @return resultado Valores resultantes || Mensaje de error
-	 */
-	function SEARCH()
-	{
-		// Sentencia SQL
-		$sql = "SELECT *
-				FROM PROFESOR
-				WHERE (
-					DNI LIKE '%".$this->dni."%' AND
-					NOMBREPROFESOR LIKE '%".$this->nombre."%' AND
-					APELLIDOSPROFESOR LIKE '%".$this->apellidos."%' AND
-					AREAPROFESOR LIKE '%".$this->area."%' AND
-					DEPARTAMENTOPROFESOR LIKE '%".$this->departamento."%'
-				)";
-
-		// Ejecutamos la sentencia y devolvemos
-		// el valor o un mensaje de error
-		if (!$resultado = $this->mysqli->query($sql))
-		{
-			return 'Error de gestor de base de datos';
 		}
-		else
+
+		/**
+		 * Busca valores en la BD
+		 * 
+		 * @return resultado Valores resultantes || Mensaje de error
+		 */
+		function SEARCH()
 		{
+			// Sentencia SQL
+			$sql = "SELECT *
+					FROM PROFESOR
+					WHERE (
+						DNI LIKE '%".$this->dni."%' AND
+						NOMBREPROFESOR LIKE '%".$this->nombre."%' AND
+						APELLIDOSPROFESOR LIKE '%".$this->apellidos."%' AND
+						AREAPROFESOR LIKE '%".$this->area."%' AND
+						DEPARTAMENTOPROFESOR LIKE '%".$this->departamento."%'
+					)";
+
+			// Ejecutamos la sentencia y devolvemos
+			// el valor o un mensaje de error
+			if (!$resultado = $this->mysqli->query($sql))
+			{
+				return 'Error de gestor de base de datos';
+			}
+			else
+			{
+				return $resultado;
+			}
+		}
+
+		/**
+		 * Borra valores de la BD
+		 * 
+		 * @return resultado Mensaje correspondiente al resultado
+		 */
+		function DELETE()
+		{
+			// Sentencia SQL
+			$sql = "DELETE FROM 
+						PROFESOR
+					WHERE(
+						DNI = '$this->dni'
+					)";
+
+			// Ejecutamos la sentencia y devolvemos el mensaje correspondiente
+			if ($this->mysqli->query($sql))
+			{
+				$resultado = 'Borrado realizado con éxito';
+			}
+			else
+			{
+				$resultado = 'Error de gestor de base de datos';
+			}
+			return $resultado;
+		}
+
+		/**
+		 * Recupera todos los atributos de una tupla a partir de su clave
+		 * 
+		 * @return tupla Valores resultantes || Mensaje de error
+		 */
+		function RellenaDatos()
+		{
+			// Sentencia SQL
+			$sql = "SELECT *
+					FROM PROFESOR
+					WHERE (
+						(DNI = '$this->dni') 
+					)";
+
+			// Ejecutamos la sentencia y devolvemos
+			// el valor o un mensaje de error
+			if (!$resultado = $this->mysqli->query($sql))
+			{
+				return 'Error de gestor de base de datos';
+			}
+			else
+			{
+				$tupla = $resultado->fetch_array();
+			}
+			return $tupla;
+		}
+
+		/**
+		 * Realizar el UPDATE de una tupla despues de comprobar que existe
+		 * 
+		 * @return resultado Mensaje correspondiente al resultado
+		 */
+		function EDIT()
+		{
+			// Sentencia SQL
+			$sql = "UPDATE PROFESOR
+					SET
+						NOMBREPROFESOR = '$this->nombre',
+						APELLIDOSPROFESOR = '$this->apellidos',
+						AREAPROFESOR = '$this->area',
+						DEPARTAMENTOPROFESOR = '$this->departamento'
+					WHERE (
+						DNI = '$this->dni'
+					)";
+			
+			// Ejecutamos la sentencia y devolvemos
+			// el mensaje correspondiente
+			if ($this->mysqli->query($sql))
+			{
+				$resultado = 'Actualización realizada con éxito';
+			}
+			else
+			{
+				$resultado = 'Error de gestor de base de datos';
+			}
 			return $resultado;
 		}
 	}
-
-	//funcion DELETE : comprueba que la tupla a borrar existe y una vez
-	// verificado la borra
-	function DELETE()
-	{
-	$sql = "DELETE FROM 
-				PROFESOR
-			WHERE(
-				DNI = '$this->dni'
-			)
-			";
-
-		if ($this->mysqli->query($sql))
-		{
-			$resultado = 'Borrado realizado con éxito';
-		}
-		else
-		{
-			$resultado = 'Error de gestor de base de datos';
-		}
-		return $resultado;
-	}
-
-	// funcion RellenaDatos: recupera todos los atributos de una tupla a partir de su clave
-	function RellenaDatos()
-	{
-		$sql = "SELECT *
-				FROM PROFESOR
-				WHERE (
-					(DNI = '$this->dni') 
-				)";
-
-		if (!$resultado = $this->mysqli->query($sql))
-		{
-				return 'Error de gestor de base de datos';
-		}else
-		{
-			$tupla = $resultado->fetch_array();
-		}
-		return $tupla;
-	}
-
-	// funcion Edit: realizar el update de una tupla despues de comprobar que existe
-	function EDIT()
-	{
-		$sql = "UPDATE PROFESOR
-				SET
-					NOMBREPROFESOR = '$this->nombre',
-					APELLIDOSPROFESOR = '$this->apellidos',
-					AREAPROFESOR = '$this->area',
-					DEPARTAMENTOPROFESOR = '$this->departamento'
-				WHERE (
-					DNI = '$this->dni'
-				)
-				";
-		if ($this->mysqli->query($sql))
-		{
-			$resultado = 'Actualización realizada con éxito';
-		}
-		else
-		{
-			$resultado = 'Error de gestor de base de datos';
-		}
-		return $resultado;
-	}
-}
-
 ?> 
