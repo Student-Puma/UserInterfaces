d791e3fc7c0172f1afd80922cfebc2266f9e634a
Gatete ROG <kikefontanlorenzo@gmail.com>
2019-10-30 23:29:54 +0100

    :bug: Fixing some little bugs

--- View/USUARIOS_DELETE_View.php
+++ View/USUARIOS_DELETE_View.php
@@ -38,35 +38,35 @@
 		<form name="Form" action="../Controller/USUARIOS_Controller.php" method="post" onsubmit="enableAll(this)">
 			<ul class="form-style">
 				<li>
-					<label><?php echo $strings['AccountData']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['AccountData']; ?></label>
 					<input type="text" readonly class="campo-dividido" id="login" name="login" placeholder="<?php echo $strings['Login']; ?>" value="<?php echo $this->tupla['login']; ?>" required>
 					<input type="password" readonly class="campo-dividido" id="password" name="password" placeholder="<?php echo $strings['Password']; ?>" value="<?php echo $this->tupla['password']; ?>" required>
 				</li>
 				<li>
-					<label><?php echo $strings['FullName']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['FullName']; ?></label>
 					<input type="text" readonly pattern="[A-Za-z][A-Za-z -]{2,29}" class="campo-dividido" id="nombre" name="nombre" placeholder="<?php echo $strings['Name']; ?>" value="<?php echo $this->tupla['nombre']; ?>" required>
 					<input type="text" readonly pattern="[A-Za-z][A-Za-z -]{2,49}" class="campo-dividido" id="apellidos" name="apellidos" placeholder="<?php echo $strings['Surname']; ?>" value="<?php echo $this->tupla['apellidos']; ?>" required>
 				</li>
 				<li>
-					<label><?php echo $strings['Email']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Email']; ?></label>
 					<input type="email" readonly pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}" class="campo-largo" id="email" name="email" placeholder="<?php echo $strings['Email']; ?>"  value="<?php echo $this->tupla['email']; ?>" required>
 				</li>
 				<li>
-					<label><?php echo $strings['Birth']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Birth']; ?></label>
 					<input type="date" readonly class="campo-largo" name="fechanac" id="fechanac" max="2018-12-31" placeholder="<?php echo $strings['Birth']; ?>" value="<?php echo $this->tupla['FechaNacimiento']; ?>" required>
 				</li>
 				<li>
-					<label ><?php echo $strings['Picture']; ?> <span class="requerido">*</span></label>
+					<label ><?php echo $strings['Picture']; ?></label>
 					<label class="file-upload readonly" id="fileuploader" for="fotopersonal"><?php echo ($this->tupla['fotopersonal'] == '' ? $strings['UploadFile'] : $this->tupla['fotopersonal']); ?></label>
 					<input disabled type="file" class="campo-largo" accept="image/*" name="fotopersonal" id="fotopersonal" required>
 				</li>
 				<li>
-					<label><?php echo $strings['PersonalData']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['PersonalData']; ?></label>
 					<input type="tel" readonly pattern="[0-9]{8}[A-NO-Za-no-z]" class="campo-dividido" id="dni" name="dni" placeholder="<?php echo $strings['DNI']; ?>" value='<?php echo $this->tupla['DNI']; ?>' required>
 					<input type="tel" readonly pattern="[9|6|7][0-9]{8}" class="campo-dividido" id="telefono" name="telefono" placeholder="<?php echo $strings['Phone']; ?>" value='<?php echo $this->tupla['telefono']; ?>' required>
 				</li>
 				<li>
-					<label><?php echo $strings['Genre']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Genre']; ?></label>
 					<select disabled class="campo-dividido" id="sexo" name="sexo" value='<?php echo $this->tupla['sexo']; ?>' required>
 						<option value="hombre" selected><?php echo $strings['Male']; ?></option>
 						<option value="mujer"><?php echo $strings['Female']; ?></option>
--- View/USUARIOS_SEARCH_View.php
+++ View/USUARIOS_SEARCH_View.php
@@ -35,35 +35,35 @@
 		<form name="Form" action="../Controller/USUARIOS_Controller.php" method="post">
 			<ul class="form-style">
 				<li>
-					<label><?php echo $strings['AccountData']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['AccountData']; ?></label>
 					<input type="text" class="campo-dividido" id="login" name="login" placeholder="<?php echo $strings['Login']; ?>">
 					<input type="password" class="campo-dividido" id="password" name="password" placeholder="<?php echo $strings['Password']; ?>">
 				</li>
 				<li>
-					<label><?php echo $strings['FullName']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['FullName']; ?></label>
 					<input type="text" pattern="[A-Za-z][A-Za-z -]{2,29}" class="campo-dividido" id="nombre" name="nombre" placeholder="<?php echo $strings['Name']; ?>">
 					<input type="text" pattern="[A-Za-z][A-Za-z -]{2,49}" class="campo-dividido" id="apellidos" name="apellidos" placeholder="<?php echo $strings['Surname']; ?>">
 				</li>
 				<li>
-					<label><?php echo $strings['Email']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Email']; ?></label>
 					<input type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}" class="campo-largo" id="email" name="email" placeholder="<?php echo $strings['Email']; ?>">
 				</li>
 				<li>
-					<label><?php echo $strings['Birth']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Birth']; ?></label>
 					<input type="date" class="campo-largo" name="fechanac" id="fechanac" max="2018-12-31" placeholder="<?php echo $strings['Birth']; ?>">
 				</li>
 				<li>
-					<label><?php echo $strings['Picture']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Picture']; ?></label>
 					<label class="file-upload" id="fileuploader" for="fotopersonal"><?php echo $strings['UploadFile']; ?></label>
 					<input type="file" class="campo-largo" accept="image/*" name="fotopersonal" id="fotopersonal">
 				</li>
 				<li>
-					<label><?php echo $strings['PersonalData']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['PersonalData']; ?></label>
 					<input type="tel" pattern="[0-9]{8}[A-NO-Za-no-z]" class="campo-dividido" id="dni" name="dni" placeholder="<?php echo $strings['DNI']; ?>">
 					<input type="tel" pattern="[9|6|7][0-9]{8}" class="campo-dividido" id="telefono" name="telefono" placeholder="<?php echo $strings['Phone']; ?>">
 				</li>
 				<li>
-					<label><?php echo $strings['Genre']; ?> <span class="requerido">*</span></label>
+					<label><?php echo $strings['Genre']; ?></label>
 					<select class="campo-dividido" id="sexo" name="sexo">
 						<option value="hombre" selected><?php echo $strings['Male']; ?></option>
 						<option value="mujer"><?php echo $strings['Female']; ?></option>
