c76671311c5dbb6d5e972af4484800278d6e3309
Gatete ROG <kikefontanlorenzo@gmail.com>
2019-12-11 15:50:12 +0100

    :sparkles: Edificio Test

--- test/EDIFICIO_test.php
+++ test/EDIFICIO_test.php
@@ -37,8 +37,8 @@
 
         // Lógica del test
 
-        $edificio = new EDIFICIO_Model($datos["COD"],$datos["NOMBRE"],$datos["DIRECCION"],$datos["CAMPUS"]);
-        $EDIFICIO_array_error['error_obtenido'] = $edificio->ADD();
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],$datos["NOMBRE"],$datos["DIRECCION"],$datos["CAMPUS"]);
+        $EDIFICIO_array_error['error_obtenido'] = $EDIFICIO->ADD();
         $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
 
         array_push($ERRORS_array_test, $EDIFICIO_array_error);
@@ -56,19 +56,200 @@
             "resultado"         => "",
         );
 
+        // Lógica del test
+
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],$datos["NOMBRE"],$datos["DIRECCION"],$datos["CAMPUS"]);
+        $EDIFICIO_array_error['error_obtenido'] = $EDIFICIO->ADD();
+        $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
+
+        array_push($ERRORS_array_test, $EDIFICIO_array_error);
+    }
+
+    /**
+     * Test unitario para la función de editar EDIFICIO
+     * 
+     * Valida:
+     *  Actualización realizada con éxito
+     */
+    function EDIFICIO_EDIT_test()
+    {
+        // Array para almacenar los errores
+        global $ERRORS_array_test;
+
+        // -------- Actualización realizada con éxito
+
+        // Plantilla
+
+        $EDIFICIO_array_error = array(
+            "entidad"           => "EDIFICIO",
+            "metodo"            => "EDIT",
+            "error"             => "Actualización correcta",
+            "error_esperado"    => "Actualización realizada con éxito",
+            "error_obtenido"    => "",
+            "resultado"         => "",
+        );
+
         // Datos usados
 
         $datos = array(
             "COD"       => "99",
-            "NOMBRE"    => "UVIGO",
+            "NOMBRE"    => "VIGO",
             "DIRECCION" => "OURENSE",
             "CAMPUS"    => "NUEVO"
         );
 
         // Lógica del test
 
-        $edificio = new EDIFICIO_Model($datos["COD"],$datos["NOMBRE"],$datos["DIRECCION"],$datos["CAMPUS"]);
-        $EDIFICIO_array_error['error_obtenido'] = $edificio->ADD();
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],$datos["NOMBRE"],$datos["DIRECCION"],$datos["CAMPUS"]);
+        $EDIFICIO_array_error['error_obtenido'] = $EDIFICIO->EDIT();
+        $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
+
+        array_push($ERRORS_array_test, $EDIFICIO_array_error);
+    }
+
+    /**
+     * Test unitario para la función de borrar EDIFICIO
+     * 
+     * Valida:
+     *  Borrado realizado con éxito
+     */
+    function EDIFICIO_DELETE_test()
+    {
+        // Array para almacenar los errores
+        global $ERRORS_array_test;
+
+        // -------- Borrado realizado con éxito
+
+        // Plantilla
+
+        $EDIFICIO_array_error = array(
+            "entidad"           => "EDIFICIO",
+            "metodo"            => "DELETE",
+            "error"             => "Borrado correcto",
+            "error_esperado"    => "Borrado realizado con éxito",
+            "error_obtenido"    => "",
+            "resultado"         => "",
+        );
+
+        // Datos usados
+
+        $datos = array(
+            "COD" => "99"
+        );
+
+        // Lógica del test
+
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],"","","");
+        $EDIFICIO_array_error['error_obtenido'] = $EDIFICIO->DELETE();
+        $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
+
+        array_push($ERRORS_array_test, $EDIFICIO_array_error);
+    }
+
+    /**
+     * Test unitario para la función de buscar EDIFICIO
+     * 
+     * Valida:
+     *  recordset
+     *  Error de gestor de base de datos
+     */
+    function EDIFICIO_SEARCH_test()
+    {
+        // Array para almacenar los errores
+        global $ERRORS_array_test;
+
+        // -------- Búsqueda correcta
+
+        // Plantilla
+
+        $EDIFICIO_array_error = array(
+            "entidad"           => "EDIFICIO",
+            "metodo"            => "SEARCH",
+            "error"             => "Búsqueda correcta",
+            "error_esperado"    => "recordset",
+            "error_obtenido"    => "",
+            "resultado"         => "",
+        );
+
+        // Datos usados
+
+        $datos = array(
+            "COD"           => "99"
+        );
+
+        // Lógica del test
+
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],"","","");
+        $resultado = $EDIFICIO->SEARCH();
+        $EDIFICIO_array_error['error_obtenido'] = !is_string($resultado) ? "recordset" : $resultado;
+        $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
+
+        array_push($ERRORS_array_test, $EDIFICIO_array_error);
+
+        // -------- Búsqueda fallida
+
+        // Plantilla
+
+        $EDIFICIO_array_error = array(
+            "entidad"           => "EDIFICIO",
+            "metodo"            => "SEARCH",
+            "error"             => "Búsqueda incorrecta",
+            "error_esperado"    => "Error de gestor de base de datos",
+            "error_obtenido"    => "",
+            "resultado"         => "",
+        );
+
+        // Datos usados
+
+        $datos = array(
+            "COD"           => "99'#"
+        );
+
+        // Lógica del test
+
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],"","","");
+        $resultado = $EDIFICIO->SEARCH();
+        $EDIFICIO_array_error['error_obtenido'] = !is_string($resultado) ? "recordset" : $resultado;
+        $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
+
+        array_push($ERRORS_array_test, $EDIFICIO_array_error);
+    }
+
+    /**
+     * Test unitario para la función de rellenar datos de EDIFICIO
+     * 
+     * Valida:
+     *  Tupla
+     */
+    function EDIFICIO_RellenaDatos_test()
+    {
+        // Array para almacenar los errores
+        global $ERRORS_array_test;
+
+        // -------- Rellenar datos realizado con éxito
+
+        // Plantilla
+
+        $EDIFICIO_array_error = array(
+            "entidad"           => "EDIFICIO",
+            "metodo"            => "RellenaDatos",
+            "error"             => "Relleno de datos correcto",
+            "error_esperado"    => "Tupla",
+            "error_obtenido"    => "",
+            "resultado"         => "",
+        );
+
+        // Datos usados
+
+        $datos = array(
+            "COD"           => "99"
+        );
+
+        // Lógica del test
+
+        $EDIFICIO = new EDIFICIO_Model($datos["COD"],"","","");
+        $resultado = $EDIFICIO->RellenaDatos();
+        $EDIFICIO_array_error['error_obtenido'] = is_array($resultado) ? "Tupla" : $resultado;
         $EDIFICIO_array_error['resultado'] = ($EDIFICIO_array_error["error_esperado"] === $EDIFICIO_array_error["error_obtenido"]) ? "OK" : "FALSE";
 
         array_push($ERRORS_array_test, $EDIFICIO_array_error);
@@ -77,4 +258,8 @@
     // REALIZAMOS LAS PRUEBAS
 
     EDIFICIO_ADD_test();
+    EDIFICIO_EDIT_test();
+    EDIFICIO_RellenaDatos_test();
+    EDIFICIO_SEARCH_test();
+    EDIFICIO_DELETE_test();
 ?>
