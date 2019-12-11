8114b55b3f941b28b633bc6b5849fa6e7d8e4cd2
Gatete ROG <kikefontanlorenzo@gmail.com>
2019-12-11 12:21:20 +0100

    :sparkles: Message View now with arrays!

--- View/MESSAGE_View.php
+++ View/MESSAGE_View.php
@@ -13,14 +13,14 @@
 	 */
 	class MESSAGE{
 		// Variables de la clase
-		private $string; 
+		private $datos; 
 		private $volver;
 
 		/**
 		 * Constructor de la clase
 		 */
-		function __construct($string, $volver){
-			$this->string = $string;
+		function __construct($datos, $volver){
+			$this->datos = $datos;
 			$this->volver = $volver;	
 			$this->render();
 		}
@@ -34,14 +34,48 @@
 			include '../View/Header.php';
 ?>
 			<div class="centrado">
-				<span class="msg"><?php echo $strings[$this->string]; ?></span>
+<?php
+			if(is_string($this->datos))
+			{
+?>
+				<span class="msg"><?php echo $strings[$this->datos]; ?></span>
+<?php
+			}
+			else
+			{
+?>
+				<table>
+					<thead>
+						<tr>
+							<th>Código</th>
+							<th>Atributo</th>
+							<th>Error</th>
+						</tr>
+					</thead>
+					<tbody>
+<?php
+				foreach($this->datos as $errores)
+				{
+?>
+						<tr>
+							<td><?php echo($errores['codigo']); ?></td>
+							<td><?php echo($errores['atributo']); ?></td>
+							<td><?php echo($errores['incidencia']); ?></td>
+						</tr>
+<?php
+				}
+?>
+					</tbody>
+				</table>
+<?php
+			}
+?>
 			</div>
 
 			<a href="<?php echo $this->volver; ?>" class="return"><?php echo $strings['Back']; ?></a>
-
 <?php
 			// Añadimos la vista Footer
 			include '../View/Footer.php';
-		}
-	}
+		} //
+	} // fin de clase
 ?>
