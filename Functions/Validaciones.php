3d9d853c6899053f6e8bd5e844bbeb4d61b5d1f1
Gatete ROG <kikefontanlorenzo@gmail.com>
2019-12-11 12:19:24 +0100

    :sparkles: Comprobar nombre

--- Functions/Validaciones.php
+++ Functions/Validaciones.php
@@ -5,19 +5,36 @@
         '00003' => 'Valor de atributo no numérico demasiado corto',
         '00004' => 'Valor de atributo numérico demasiado corto',
         '00010' => 'Formato dni erróneo',
-        '00011' => 'Dni no válido'
+        '00011' => 'Dni no válido',
+        '00030' => 'Solo están permitidas alfabéticos'
     );
 
     function validarLetraDNI($dni)
     {
         $letra = strtoupper(substr($dni, -1));
-        $numeros = substr($dni, 0, -1);
+        $numeros = intval(substr($dni, 0, -1));
         return substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros % 23, 1) === $letra;
+    }
+
+    function comprobar_nombre($nombre)
+    {
+        $atributo = "nombre";
+        $errores = array();
+
+        if(empty($nombre))
+            { array_push($errores,array('atributo' => $atributo, 'codigo' => '00001', 'incidencia' => $GLOBALS['INCIDENCIAS']['00001'])); }
+        if(strlen($nombre) > 15)
+            { array_push($errores,array('atributo' => $atributo, 'codigo' => '00002', 'incidencia' => $GLOBALS['INCIDENCIAS']['00002'])); }
+        if(strlen($nombre) < 3)
+            { array_push($errores,array('atributo' => $atributo, 'codigo' => '00003', 'incidencia' => $GLOBALS['INCIDENCIAS']['00003'])); }
+        if(!preg_match('/[A-Za-z][A-Za-z -]{2,14}/',$nombre))
+            { array_push($errores,array('atributo' => $atributo, 'codigo' => '00030', 'incidencia' => $GLOBALS['INCIDENCIAS']['00030'])); }
+
+        return empty($errores) ? true : $errores;
     }
 
     function comprobar_DNI($dni)
     {
-
         $atributo = "dni";
         $errores = array();
 
@@ -34,5 +51,4 @@
 
         return empty($errores) ? true : $errores;
     }
-
 ?>
